ec7554f93fb476f9ca858a1088e69d4416bb84fb
Henry Chin <hchin3@uncc.edu>
2023-05-11 05:21:03 -0400

    added search test

--- tests/e2e/test_search.py
+++
@@ -1,4 +0,0 @@
-from tests.e2e.conftest import test_app
-
-def test_load_maps(test_app):
-    response = test_app.get('/maps')
--- tests/test_restroom_routes.py
+++ tests/test_restroom_routes.py
@@ -30,3 +30,32 @@ def test_delete_rating(test_client):
     # Clear database
     Rating.query.delete()
     db.session.commit()
+
+def test_search_rating(test_client):
+
+    Rating.query.delete()
+    db.session.commit()
+
+    temp_rating1 = Rating(rater_id=1, restroom_name="Testroom1", cleanliness=3.0, overall=3.5)
+    temp_rating2 = Rating(rater_id=2, restroom_name="Testroom 2", cleanliness=2.0, overall=2.5)
+    db.session.add(temp_rating1)
+    db.session.add(temp_rating2)
+    db.session.commit()
+
+    res = test_client.get('/search/?searchbox=Test')
+    data = res.data.decode()
+
+    #assert res.status_code == 200
+    assert '<h5 class="card-title">Testroom1</h5>' in data
+    assert '<h5 class="card-title">Testroom 2</h5>' in data
+    assert '<div class="alert alert-danger" role="alert">No restrooms found!</div>' not in data
+
+    res = test_client.get('/search/?searchbox=NotInDB')
+    data = res.data.decode()
+
+    assert '<h5 class="card-title">Testroom1</h5>' not in data
+    assert '<h5 class="card-title">Testroom 2</h5>' not in data
+    assert '<div class="alert alert-danger" role="alert">No restrooms found!</div>' in data
+
+
+
